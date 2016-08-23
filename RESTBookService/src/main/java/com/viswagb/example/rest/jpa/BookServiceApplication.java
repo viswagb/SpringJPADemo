89dfd0e3f76a245fe0dc26f2a55052d8c71f2be7
Viswanatha.Basavalinga <viswanatha.basavalingappa@oracle.com>
2016-08-23 11:03:26 -0400

    demo copy

---
+++ RESTBookService/src/main/java/com/viswagb/example/rest/jpa/BookServiceApplication.java
@@ -0,0 +1,12 @@
+package com.viswagb.example.rest.jpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookServiceApplication {
+    
+    public static void main( String[] args ){
+        SpringApplication.run(BookServiceApplication.class);
+    }
+} 
